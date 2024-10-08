6d5d129bf16ebea4ab1e2741856c247f581d08dd
Cristianf96 <cb961218@gmail.com>
2024-10-08 15:53:52 -0500

    First Commit Dev

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,19 +1,14 @@
-interface GenerateDictionaryParams {
-  typeResource: string;
-  data: any;
-  keyValue: string;
-  secretName: string;
-}
+import { GenerateDictionaryParams } from "@app/app/utils/interfaces/mssql.interface";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 export class MDocsDep {
   generateDictionary(params: GenerateDictionaryParams): Record<string, any> {
-    const { typeResource, data, keyValue, secretName } = params;
+    const { typeResource, data } = params;
 
     return {
       typeResource,
       data,
-      keyValue,
-      secretName,
     };
   }
 }
---
+++ src/app/utils/interfaces/mssql.interface.ts
@@ -0,0 +1,4 @@
+export interface GenerateDictionaryParams {
+  typeResource: string;
+  data: any;
+}
